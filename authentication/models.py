d0632fb9b9e77519af6e11f02a44e7200c12f75d
ckabah <coulb.password>
2022-10-21 01:59:26 +0100

    adding user model in authentication app and its test

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils import timezone
 
-# Create your models here.
+class AcountManager(BaseUserManager):
+
+    def create_user(self, email, user_name, password=None, **kwargs):
+        if not email:
+            raise ValueError('Users must have an email adress')
+        user = self.model(
+            email = self.normalize_email(email), 
+            user_name = user_name)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email,user_name, password):
+        user = self.create_user(email, password, user_name)
+        user.is_active = True
+        user.is_admin = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique = True)
+    user_name = models.CharField(max_length = 50)
+    date_joined = models.DateTimeField(default = timezone.now)
+    last_login = models.DateTimeField(null=True)
+    is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default = False)
+    is_superuser = models.BooleanField(default = False)
+
+    objects = AcountManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name']
+
+    def __str__(self) -> str:
+        return self.user_name
+    
+    def get_short_name(self):
+        return self.user_name[0]
---
+++ authentication/tests/test_model.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from authentication.models import User
+
+class UserModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(self):
+        User.objects.create_user(email='cs.ttrx@gmail.com', user_name='csttrx', password='coull1515fdf')
+        User.objects.create_superuser('coul@gmail.com', user_name='cscoul', password='godejeroien583')
+
+    def test_user_name_label(self):
+        user = User.objects.get(id = 1)
+        user_name_label = user._meta.get_field('user_name').verbose_name
+        self.assertEqual(user_name_label, 'user name')
+
+    def test_user_name_max_length(self):
+        user = User.objects.get(id = 1)
+        max_length = user._meta.get_field('user_name').max_length
+        self.assertEqual(max_length, 50)
+    
+    def test_user_is_active_admin_superuser(self):
+        user = User.objects.get(id = 1)
+        superuser = User.objects.get(id=2)
+        self.assertFalse(user.is_active)
+        self.assertFalse(user.is_admin)
+        self.assertFalse(user.is_superuser)
+        self.assertTrue(superuser.is_active)
+        self.assertTrue(superuser.is_admin)
+        self.assertTrue(superuser.is_superuser)
+    
+    def test_user_name(self):
+        user = User.objects.get(id=1)
+        self.assertEqual(user.get_short_name(), 'c')
